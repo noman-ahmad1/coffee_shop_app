070ebd4b9644eaea41fd967507184f89cfa0d855
Noman Ahmad <nomanamjad105@gmail.com>
2025-02-03 19:16:23 +0500

    firebase_integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -5,12 +6,15 @@ import 'package:my_first_app/app/app.bottomsheets.dart';
 import 'package:my_first_app/app/app.dialogs.dart';
 import 'package:my_first_app/app/app.locator.dart';
 import 'package:my_first_app/app/app.router.dart';
+import 'package:my_first_app/firebase_options.dart';
 import 'package:stacked_services/stacked_services.dart';
-
 import 'ui/models.dart/coffee_model.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   await Hive.initFlutter();
   Hive.registerAdapter(CoffeeFlavorAdapter()); // Register the adapter
   await Hive.openBox('cart');
